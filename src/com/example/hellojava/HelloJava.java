311d18fe6dc5d6e9931c925a1d6ef922ec1a2035
Angelo Fioretti <angelo.fioretti@gmail.com>
2024-01-02 12:16:55 -0700

    1st

---
+++ src/com/example/hellojava/HelloJava.java
@@ -0,0 +1,9 @@
+package com.example.hellojava;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.print("Hello Java");
+        System.out.println(" " +
+                "Hello again");
+    }
+}
